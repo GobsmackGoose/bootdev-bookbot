8e06c32c970c237e07e0ec844cdc30d29b99d12b
werner-goosen <werner.goosen@protonmail.com>
2025-01-09 00:28:14 +0200

    updated reporting with breakdown of characters

--- main.py
+++ main.py
@@ -19,6 +19,25 @@ error_codes = {
 }
 
 #NOTE(wernerg): Functions
+def sort_pair(result, by_key=False, reverse=False):
+  key_func = (lambda x: x[0]) if by_key else (lambda x: x[1])
+  result.sort(key = key_func, reverse = reverse)
+  return result
+
+def count_per_char(text):
+  result = []
+  for char in text.lower():
+    if char.isalpha():
+      found = False
+      for i, (c, cnt) in enumerate(result):
+        if c == char:
+          result[i] = (c, cnt + 1)
+          found = True
+          break
+      if not found:
+        result.append((char, 1))
+  return result
+
 def format_error_message(error_code, msg):
   result = ""
   if PROGRAM_MODE_DEV in program_modes:
@@ -59,16 +78,23 @@ def main():
   #NOTE(wernerg): Processing section
   err, file_content = read_file(filepath)
   if err: result = append_log(err, file_content)
-  words = file_content.split()
-  count = len(words)
+  total_char_cnt = len(file_content)
+  cnt_pair = count_per_char(file_content)
+  sorted_pair = sort_pair(cnt_pair, reverse=True)
+  total_word_cnt = len(file_content.split())
 
   #NOTE(wernerg): Output section
   if not err:
     if PROGRAM_MODE_CLI in program_modes:
       result.append(file_content)
-    if PROGRAM_MODE_STATS  in program_modes:
-      result.append(f"Total word count: {count}")
-
+    if PROGRAM_MODE_STATS in program_modes:
+      result.append(f"--- Analysis report of {filepath} ---")
+      result.append(f"Total word count: {total_word_cnt}")
+      if sorted_pair is not None:
+        result.append(f"Total char count: {total_char_cnt}")
+        for char, cnt in sorted_pair:
+          result.append(f"'{char}': {cnt}")
+      result.append(f"--- End of analysis report ---")
   if len(result) > 0: print("\n".join(filter(None, result)))
 
 if __name__ == "__main__":
